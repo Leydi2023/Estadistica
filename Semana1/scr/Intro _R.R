db404923e373ddad4bff3a79fd673684bd3917a3
Leydi Diaz Avila <56773014+Leydi2023@users.noreply.github.com>
2019-10-19 23:11:31 -0500

    Create Intro _R.R

---
+++ Semana1/scr/Intro _R.R
@@ -0,0 +1,10 @@
+#Desarrollo ejercicios 1 semana 
+
+**Ejer:** Reasigne 5 a la letra *z*, usando el operador *<-*, ahora, pregunte ¿qué es *z*?
+4 -> z
+
+z <- 5
+
+z
+
+**Ejer:** Revisemos qué es rnorm(1). Empecemos buscando la función en help o ?rnorm
